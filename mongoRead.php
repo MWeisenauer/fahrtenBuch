f6876d46e8c84cf046eeb93886933a2e39d6f3b1
MWeisenauer <39963125+MWeisenauer@users.noreply.github.com>
2019-08-27 14:59:48 +0200

    Zurück

--- mongoRead.php
+++ mongoRead.php
@@ -3,10 +3,10 @@
 <head>
 	<title>Read DB</title>
 </head>
-<body> 
+<body>
  <?php
   error_reporting (E_ALL);
-  echo "Ich bin oben.."."<br>"; Hättest du gerne
+  echo "Ich bin oben.."."<br>"; 
   //$m = new MongoDB\Driver\Manager('mongodb://localhost:27017');
   $m = new MongoDB\Driver\Manager('mongodb+srv://MWeisenauer:Adenauer@timetec-bzlfs.mongodb.net/test?retryWrites=true');
 
